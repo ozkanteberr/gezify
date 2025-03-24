c2e187dd2d61e86f827919a5c1ace30160dc2875
ozkanteber <teberozkan@gmail.com>
2025-03-24 10:51:36 +0300

    splash screen eklendi,giriş sayfası oluşturuldu.

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String base = 'assets/images/';
+
+  static const String appLogo = '${base}app_logo.png';
+  static const String appTitle = '${base}app_title_rm.png';
+}
