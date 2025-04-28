67b5c912f151e831ee1a976df6cc585a6485b3f2
CagatayTurunc <cagatayturunc@gmail.com>
2025-04-28 21:45:04 +0300

    HomePage tasarlandı

---
+++ lib/presentation/home/pages/home_page.dart
@@ -0,0 +1,138 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:gezify/common/widgets/app_bar.dart';
+import 'package:gezify/common/widgets/destination_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Ana Sayfa'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Takvim'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Mesajlar'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Üst kısım: Profil ve Bildirim
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 25,
+                        backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'Kullanıcı Adı',
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[700],
+                            ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.notifications_none),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // "Explore the Beautiful world!" yazısı
+             Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                    'Explore the',
+                     style: TextStyle(
+                     fontWeight: FontWeight.w500,
+                     fontSize: 28,
+                     color: Colors.black,
+                     ),
+                      ),
+                   RichText(
+                   text: TextSpan(
+                   style: TextStyle(fontSize: 28),
+                   children: [
+                    TextSpan(
+                    text: 'Beautiful ',
+                    style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                      ),
+                       ),
+                    TextSpan(
+                    text: 'world!',
+                    style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.bold,
+                   ),
+                  ),
+                 ],
+                ),
+               ),
+              ],
+             ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Best Destination',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal,color: Colors.black),
+                    
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('View all'),
+                  )
+                ],
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    DestinationCard(
+                    imageUrl: '',
+                    title: 'Niladri Reservoir', 
+                    location: 'Trabzon, Türkiye', 
+                    rating: 4.7,
+                    ),
+                     const DestinationCard(
+                      imageUrl: 'https://c4.wallpaperflare.com/wallpaper/169/815/806/forest-hills-lake-landscape-wallpaper-preview.jpg',
+                      title: 'Darma Valley',
+                      location: 'Darma, India',
+                      rating: 4.8,
+                    ),
+                    
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
